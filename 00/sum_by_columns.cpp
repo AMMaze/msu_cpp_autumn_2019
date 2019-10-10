97b746c87028ab4d2fa2c93e2aab3f4cd327ab91
Marat Abbas <unlock96@gmail.com>
2019-10-10 08:10:34 +0300

    hw1

---
+++ 00/sum_by_columns.cpp
@@ -0,0 +1,19 @@
+#include "timer.h"
+
+int main() {
+    const int n = 1000;
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            arr[i][j] = i * j;
+
+    Timer t;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            sum += arr[j][i];
+
+    return 0;
+}
