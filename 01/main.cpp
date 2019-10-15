763081036ad9ea2d42ca91a84cd435c6812344b0
Marat Abbas <unlock96@gmail.com>
2019-10-15 15:08:13 +0300

    added error msg

--- 01/main.cpp
+++ 01/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
 
     if (argc != 2) {
-        std::cout << "Incorrect number of arguments. Must be 2"
+        std::cout << "Incorrect number of arguments. Must be 2\n";
         return 1;
     }
 
